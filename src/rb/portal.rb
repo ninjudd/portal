de0a1b7c400be648834136801e279bd764803d26
ninjudd <git@justinbalthrop.com>
2011-05-11 11:25:33 -0700

    ocd

--- src/rb/portal.rb
+++ src/rb/portal.rb
@@ -5,48 +5,47 @@ class Portal
   class ProtocolError < StandardError; end
   RESULT_WAIT = 0.01
 
-  def initialize( port, host = "localhost")
-    @socket = TCPSocket.new( host, port )
+  def initialize(port, host = "localhost")
+    @socket = TCPSocket.new(host, port)
     @contexts = {}
     Thread.new do
-      while ( message = receive_message )
+      while (message = receive_message)
         id, type, form = message
-        if [":stdout", ":stderr"].include?( type )
-          out = context( id )[type][1]
-          out.write( form )
+        if [":stdout", ":stderr"].include?(type)
+          out = context(id)[type][1]
+          out.write(form)
           out.flush
         else
-          context( id )[:results] << [type, form]
+          context(id)[:results] << [type, form]
         end
       end
     end
   end
 
-  def stringify( o )
-    o.kind_if?( Symbol ) ? ":#{o.to_s}" : o.to_s
+  def stringify(obj)
+    obj.kind_of?(Symbol) ? ":#{obj}" : obj.to_s
   end
 
-  def send_message( id, type, form )
-    type = ":#{type}" if type.kind_of?( Symbol )
-    message = "#{id} #{type} #{form}"
-    @socket.write( "#{message.size}:" )
-    @socket.write( message )
-    @socket.write( "," )
+  def send_message(id, type, form)
+    message = "#{stringify(id)} #{stringify(type)} #{form}"
+    @socket.write("#{message.size}:")
+    @socket.write(message)
+    @socket.write(",")
     @socket.flush
   end
 
   def receive_message
     size = ""
-    while ( c = @socket.getc ) != ?:
-      raise ProtocolError.new( "Message size must be an integer, found #{c.chr}" ) unless (?0 .. ?9).include?( c )
+    while (c = @socket.getc) != ?:
+      raise ProtocolError.new("Message size must be an integer, found #{c.chr}") unless (?0..?9).include?(c)
       size << c
     end
-    id, type, form = @socket.read( size.to_i ).split( /\s+/, 3 )
-    raise ProtocolError.new( "Message must be followed by comma" ) unless @socket.getc == ?,
+    id, type, form = @socket.read(size.to_i).split(/\s+/, 3)
+    raise ProtocolError.new("Message must be followed by comma") unless @socket.getc == ?,
     [id, type, form]
   end
 
-  def context( id )
+  def context(id)
     @contexts[id] ||= {
       :results => [],
       :count   => 0,
@@ -55,24 +54,23 @@ class Portal
     }
   end
 
-  def with_context( id )
+  def with_context(id)
     old_id, @id = @id, id
     yield
   ensure
     @id = old_id
   end
 
-  def eval( form, id = @id || rand )
+  def eval(form, id = @id || rand)
     id = id.to_s
-    send_message( id, :eval, form)
-    context = context( id )
+    send_message(id, :eval, form)
+    context = context(id)
     count   = context[:count] += 1;
     lambda do
-      while ( count > context[:results].size )
-        sleep( RESULT_WAIT )
+      while (count > context[:results].size)
+        sleep(RESULT_WAIT)
       end
       context[:results][count - 1]
     end
   end
 end
-
