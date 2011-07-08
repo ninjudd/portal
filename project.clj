311ec5a27b71d9555bbd6a1d6d9bae8538cb87d6
Justin Balthrop <git@justinbalthrop.com>
2011-07-07 23:01:42 -0700

    update to aleph 0.2.0-alpha2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject portal "0.0.1-SNAPSHOT"
+(defproject portal "0.0.1"
   :description "The cake is a lie!"
   :dependencies [[clojure "1.2.0"]
                  [useful "0.4.0"]
-                 [aleph "0.1.5-SNAPSHOT"]])
+                 [aleph "0.2.0-alpha2"]])
