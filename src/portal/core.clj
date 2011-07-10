493208e767ccfa1b1acaf04b869dadb674a21e97
Justin Balthrop <git@justinbalthrop.com>
2011-07-09 21:39:36 -0700

    remove unused code

--- src/portal/core.clj
+++ src/portal/core.clj
@@ -2,14 +2,6 @@
   (:use gloss.core lamina.core
         [clojure.string :only [join split]]))
 
-(defn read-seq [string]
-  (with-in-str string
-    (loop [forms []]
-      (let [form (read *in* false ::EOF)]
-        (if (= ::EOF form)
-          forms
-          (recur (conj forms form)))))))
-
 (def message
   (compile-frame
    (string :utf-8 :length (prefix (string-integer :ascii :delimiters [":"]) inc dec))
